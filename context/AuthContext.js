f50942516dccf28270210b4949f445df3568cbc9
manutdmohit <saudmohit@gmail.com>
2021-11-07 10:21:43 +0545

    store jwt in server httponly cookie

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -27,7 +27,7 @@ export const AuthProvider = ({ children }) => {
    })
 
   const data = await res.json();
-    console.log(data);
+
 
   if(res.ok){
     setUser(data.user)
--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -22,9 +22,11 @@ import { FaImage } from 'react-icons/fa';
 //   };
 // }
 
-export async function getServerSideProps({ params: { id } }) {
+export async function getServerSideProps({ params: { id },req }) {
   const res = await fetch(`${API_URL}/events/${id}`);
   const evt = await res.json();
+
+  console.log(req.headers.cookie);
 
   return {
     props: { evt },
