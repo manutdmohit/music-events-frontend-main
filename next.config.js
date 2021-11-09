b494bf4d71580a512aaee271dc993707b2067744
manutdmohit <saudmohit@gmail.com>
2021-11-09 23:32:52 +0545

    solving cors error

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ module.exports = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.example.com/:path*',
+      },
+    ];
+  },
 };
